199de0f082e6d2a144185f36d142d2e38f2ecc49
Edwin Franco Sanchez <edfrancos@unal.edu.co>
2022-11-13 00:09:24 -0500

    Some changes were made in the project setup

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ module.exports = {
 	mode: 'development',
 	entry: './src/js/index.js',
 	output: {
-		assetModuleFilename: 'images/[name]-[hash].[ext]',
+		assetModuleFilename: 'img/[name]-[hash].[ext]',
 	},
 	module: {
 		rules: [{ test: /\.html$/, loader: 'html-loader' }],
